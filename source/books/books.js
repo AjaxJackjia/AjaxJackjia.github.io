2b1ebf507611be18602eac41df63a60370573220
Jack Jia <jackjianju@gmail.com>
2017-02-19 17:24:04 +0800

    add book menu item to record reading history

---
+++ source/books/books.js
@@ -0,0 +1,26 @@
+/*
+ * The name, photo and link are required attributes of each book item.
+ */
+
+BOOKS_OF_READING = [
+	{
+		'name': '金字塔原理',
+		'photo': '/uploads/books/pyramid-principle.jpg',
+		'link': 'https://book.douban.com/subject/1020644/',
+		'desc': '',
+		'start_time': '',
+		'end_time': '',
+	},
+];
+
+BOOKS_OF_READ = [
+	{
+		'name': '穆斯林的葬礼',
+		'photo': '/uploads/books/穆斯林的葬礼.jpg',
+		'link': 'https://book.douban.com/subject/2244146/',
+		'desc': '',
+		'start_time': '',
+		'end_time': '',
+	}
+];
+
---
+++ source/books/index.md
@@ -0,0 +1,16 @@
+title: "Book"
+date: 2017-02-19 15:25:00
+comments: false
+---
+
+# 在读
+<div id="reading"></div>
+
+# 已读
+<div id="read"></div>
+
+
+<!-- Book Item Generate Begin -->
+<script src="books.js"></script>
+<script src="index.js"></script>
+<!-- Book Item Generate End -->
