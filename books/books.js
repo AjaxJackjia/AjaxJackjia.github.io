3294bd366f56e65ced705133d5a2c619d7248af6
Jack Jia <jackjianju@gmail.com>
2017-04-08 15:46:47 +0800

    Site updated: 2017-04-08 15:46:47

---
+++ books/books.js
@@ -0,0 +1,34 @@
+/*
+ * The name, photo and link are required attributes of each book item.
+ */
+
+BOOKS_OF_READING = [
+    {
+		'name': '黑客与画家',
+		'photo': '/uploads/books/hackers-and-painters.jpg',
+		'link': 'https://book.douban.com/subject/6021440/',
+		'desc': '',
+		'start_time': '',
+		'end_time': '',
+	},
+];
+
+BOOKS_OF_READ = [
+	{
+		'name': '金字塔原理',
+		'photo': '/uploads/books/pyramid-principle.jpg',
+		'link': 'https://book.douban.com/subject/1020644/',
+		'desc': '',
+		'start_time': '',
+		'end_time': '',
+	},
+	{
+		'name': '穆斯林的葬礼',
+		'photo': '/uploads/books/穆斯林的葬礼.jpg',
+		'link': 'https://book.douban.com/subject/2244146/',
+		'desc': '',
+		'start_time': '',
+		'end_time': '',
+	}
+];
+
---
+++ books/index.js
@@ -0,0 +1,46 @@
+window.onload = function () {
+  var genBookItem = function(item) {
+		var $img = $("<img>");
+		$img.css("width", "107px");
+		$img.attr("src", "http://7xk4nm.com1.z0.glb.clouddn.com" + item['photo']);
+		$img.attr("alt", item['name']);
+
+		var $item = $('<a target="_blank"></a>');
+		$item.css("display", "inline-block");
+		$item.css("border-bottom", "none");
+		$item.css("padding-right", "20px");
+		$item.css("padding-top", "20px");
+		$item.css("width", "107px");
+		$item.attr("href", item['link']);
+
+		$item.append($img);
+		return $item;
+	};
+
+	var init = function() {
+		//adjust title style
+		$("#reading").css("margin-top", "-10px");
+		$("#reading").css("margin-bottom", "30px");
+		$("#read").css("margin-top", "-10px");
+		$("#read").css("margin-bottom", "30px");
+
+		//generate reading
+		for (index in BOOKS_OF_READING) {
+			var bookItem = genBookItem(BOOKS_OF_READING[index]);
+			$("#reading").append(bookItem);
+		}
+
+		//generate read
+		for (index in BOOKS_OF_READ) {
+			var bookItem = genBookItem(BOOKS_OF_READ[index]);
+			$("#read").append(bookItem);
+		}
+	};
+
+	init();
+};
+
+
+	
+	
+
